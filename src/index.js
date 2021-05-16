eb50df0ce82a28af773a435b093688de42fae598
Makrand Ghadigaonkar <80622269+MakrandGhadigaonkar@users.noreply.github.com>
2021-05-16 20:58:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.render(
       domain={dom}
       clientId={id}
       redirectUri={window.location.origin}
-      catchLocation='localStorage'
+      catchLocation='localstorage'
     >
       <GithubProvider>
         <App />
